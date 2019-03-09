2b09d16267067740f99c3dfe1f694c05242484a2
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-09 13:41:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,32 +17,25 @@ def move(board, index, character = "X")
 end
 
 
-def valid_move?(board, index)
-  if (index.between?(0,8) == true) && (position_taken?(board, index) == false)
-    true
+def position_taken?(board, index)
+  if !(board[index] == "" || board[index] == " " || board[index] == nil)
+    return true
   else
-    false
+    return false
   end
 end
 
 
- def position_taken?(board, index)
-   if board[index] == " " || board[index] == "" || board[index] == nil
-     false
-   else
-     true
-   end
- end
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
 
 
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
+  puts "Please enter 1-9"
+  
 end
